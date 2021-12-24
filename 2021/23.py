81e45fa1e22855a363df656762348d9d241fa2f0
Sebastian Strug <strugsebastian@gmail.com>
2021-12-24 17:32:40 +0000

    23 sol

---
+++ 2021/23.py
@@ -0,0 +1,7 @@
+# part 1 was 14460
+# done by hand on a chessboard. Bane of my life.
+
+# also did part 2 by hand. Used https://aochelper2021.blob.core.windows.net/day23/index.html
+# With the insert it looked like there were only 1 or 2 possible solutions,
+# I tried a few times and submitted my first solution
+# which turned out to be optimal. lucky me.
